18ef2847389182c9bcd1051dd7730faa5f79998a
CyanBlaster <michael.ng.do@gmail.com>
2026-06-26 23:17:49 -0400

    Started creating a functioning high score system, as well as finishing the transition from game to lose screen & vice versa

--- Scenes/lose_scene.gd
+++ Scenes/lose_scene.gd
@@ -3,3 +3,6 @@ extends Node2D
 func _process(_delta: float) -> void:
 	if(Input.is_action_just_pressed("shoot")):
 		get_tree().change_scene_to_file("res://Scenes/battle_scene.tscn")
+		health.health = 100
+		health.highscore = health.kills
+		health.kills = 0 
--- health.gd
+++ health.gd
@@ -2,3 +2,4 @@ extends Node
 
 var health = 100
 var kills = 0
+var highscore = 0
