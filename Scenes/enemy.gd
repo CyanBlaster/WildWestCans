e1350454c7224b76aa82a609df60e2b3d3fefca4
CyanBlaster <michael.ng.do@gmail.com>
2026-06-27 11:20:54 -0400

    Started adding a death pose for enemies

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -2,10 +2,12 @@ extends Node2D
 @onready var player = Node2D
 @export var idle : Sprite2D
 @export var shoot : Sprite2D
+@export var dead : Sprite2D
 
 var killing = 0
 var timer = 2 - 0.01 * health.kills;
 var fire = 0;
+var hp = 1;
 
 func _ready() -> void:
 	killing = 1
@@ -17,17 +19,22 @@ func _process(delta: float) -> void:
 	if(Input.is_action_just_pressed("shoot")):
 		if(position.x >= get_global_mouse_position().x - 25 && position.x <= get_global_mouse_position().x + 25
 		 && position.y >= get_global_mouse_position().y - 35 && position.y <= get_global_mouse_position().y + 35):
+			hp -= 1;
 			queue_free()
 			health.kills += 1
 			killing = 0
 	timer -= delta
+	if(hp <= 0):
+		dead.show()
 	if(fire >= 0):
 		shoot.show()
 		idle.hide()
+		dead.hide()
 		fire -= delta
 	else:
 		shoot.hide()
 		idle.show()
+		dead.hide()
 	if(timer <= 0):
 		fire = 0.1;
 		health.health -= 5 * killing
--- Scenes/lose_scene.gd
+++ Scenes/lose_scene.gd
@@ -4,5 +4,6 @@ func _process(_delta: float) -> void:
 	if(Input.is_action_just_pressed("shoot")):
 		get_tree().change_scene_to_file("res://Scenes/battle_scene.tscn")
 		health.health = 100
-		health.highscore = health.kills
+		if(health.kills > health.highscore):
+			health.highscore = health.kills
 		health.kills = 0 
